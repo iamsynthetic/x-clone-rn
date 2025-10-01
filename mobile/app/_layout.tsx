7b5ffd9e84252bef54266bc400ec582a6e74f58b
Chris Tomotsugu <tomo@Chriss-MacBook-Pro.local>
2025-09-30 22:24:07 -0400

    search tab added

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,10 +1,14 @@
 import { Stack } from "expo-router";
 import "../global.css";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Home" }} />
-    </Stack>
+    <ClerkProvider tokenCache={tokenCache}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      </Stack>
+    </ClerkProvider>
   );
 }
